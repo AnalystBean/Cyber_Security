3d04c3d11a01810003de53fea0b32e9aba32d323
Mikerosoftpro <bean.michael2528@gmail.com>
2023-02-12 09:57:00 -0500

    Update Honeypot_Example.py
    Added Logging and Error Handling

--- Honeypot_Example.py
+++ Honeypot_Example.py
@@ -1,26 +1,47 @@
 import socket
+import logging
 
 def start_honeypot(port):
 
+    logging.basicConfig(filename='honeypot.log', level=logging.INFO,
+                        format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    s.bind(('0.0.0.0', port))
+    try:
+        s.bind(('0.0.0.0', port))
+    except socket.error as e:
+        logging.error(f"Failed to bind socket: {e}")
+        return
 
     s.listen(1)
-
-    print(f"Honeypot listening on port {port}")
+    logging.info(f"Honeypot listening on port {port}")
 
     while True:
+        try:
+            conn, addr = s.accept()
+            logging.info(f"Received connection from {addr[0]}")
+        except socket.error as e:
+            logging.error(f"Failed to accept incoming connection: {e}")
+            continue
 
-        conn, addr = s.accept()
-        print(f"Received connection from {addr[0]}")
+        try:
+            data = conn.recv(1024)
+            logging.info(f"Received data: {data}")
+        except socket.error as e:
+            logging.error(f"Failed to receive data: {e}")
+            continue
 
-        data = conn.recv(1024)
-        print(f"Received data: {data}")
+        try:
+            conn.send(b"This is a honeypot, please do not attempt to exploit it.")
+        except socket.error as e:
+            logging.error(f"Failed to send response: {e}")
+            continue
 
-        conn.send(b"This is a honeypot, please do not attempt to exploit it.")
-
-        conn.close()
-
+        try:
+            conn.close()
+        except socket.error as e:
+            logging.error(f"Failed to close connection: {e}")
+            continue
 
 start_honeypot(80)
